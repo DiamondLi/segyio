a531fbf8fddb514d1f743178e36e2ccff25248ba
DiamondLi <dyc5810911@126.com>
2016-09-25 21:18:00 +0800

    Add files via upload
    first commit

---
+++ SegyIO.cpp
@@ -0,0 +1,37 @@
+/***********************************************************************************
+Author  : yang.deng
+Email   : yang.deng@geotomo.com
+Version : v1.0
+Date    : 2016.09.09
+copyright@reverse 2016 GeoTomo.Hefei
+************************************************************************************/
+
+#include "SegyIO.h"
+
+CSegyIO::CSegyIO(std::string strSEGYFile, bool _bigEdit) : segyFile(strSEGYFile),
+	bigEdit(_bigEdit),isOpen(false),id(0),sampleInterval(0),traceNumber(0),
+	sampleNumber(0), format(0), trace(nullptr), tHeaders(nullptr){
+	memset(fHeader, 0, sizeof(fHeader));
+	memset(tHeader, 0, sizeof(tHeader));
+}
+
+void CSegyIO::switchByte(char* const c,const int &len)
+{
+	int index;char tmp;
+	for (index = 0; index < len / 2; index++)
+	{
+		tmp = c[index];
+		c[index] = c[len - index - 1];
+		c[len - index - 1] = tmp;
+	}
+}
+
+bool CSegyIO::open(){ return true; }
+
+void CSegyIO::close(){}
+
+CSegyIO::~CSegyIO()
+{
+	if (trace){ delete[]trace; trace = nullptr; }
+	if (tHeaders){ delete[]tHeaders; tHeaders = nullptr; }
+}
